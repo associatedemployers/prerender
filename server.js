e39c187802ecb31500208663a8b6f78b8d99cc66
James <james@aehr.org>
2014-04-14 09:07:33 -0600

    Some custom changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var prerender = require('./lib')
 
 var server = prerender({
     workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
-    iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
+    iterations: process.env.PHANTOM_WORKER_ITERATIONS || 4,
     phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
     messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
 });
@@ -12,7 +12,7 @@ var server = prerender({
 // server.use(prerender.basicAuth());
 // server.use(prerender.whitelist());
 server.use(prerender.blacklist());
-// server.use(prerender.logger());
+server.use(prerender.logger());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 // server.use(prerender.inMemoryHtmlCache());
