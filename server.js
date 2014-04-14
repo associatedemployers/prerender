72fb8e7e7ebc6a3446850b63ff53ed223a6a0c01
James <james@aehr.org>
2014-04-14 11:16:47 -0600

    Defaults

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 var prerender = require('./lib')
 
 var server = prerender({
-    workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
-    iterations: process.env.PHANTOM_WORKER_ITERATIONS || 4,
-    phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
+    workers: 2,
+    iterations: 4,
+    phantomBasePort: 12300,
     messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
 });
 
