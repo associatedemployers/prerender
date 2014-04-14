e07465cdbdc85cd562a97c4977cce44ed64aa2c0
James <james@aehr.org>
2014-04-14 11:37:11 -0600

    Switch this default

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var prerender = require('./lib')
 
 var server = prerender({
     workers: 2,
-    iterations: 4,
+    iterations: 200,
     phantomBasePort: 12300,
     messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
 });
